c2759f55121aff731172b7dcac7f08c36291a2cf
ThVerdam <thomasverdam@hotmail.com>
2019-09-14 08:08:53 -0300

    JS ready, starting the css

---
+++ animation.js
@@ -0,0 +1,48 @@
+var inputElement = document.querySelector('#container input');
+var buttonElement = document.querySelector('#container button');
+var listElement = document.querySelector('#container ul');
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || ['List', 'Your', 'ToDo'];
+
+function renderTodos() {
+    listElement.innerHTML = '';
+
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+        var linkElement = document.createElement('a');
+        var linkText = document.createTextNode('Excluir');
+        var pos = todos.indexOf(todo);
+
+        linkElement.setAttribute('href', '#');
+        linkElement.setAttribute('onclick', 'removeTodo(' + pos + ')');
+        linkElement.appendChild(linkText);
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+
+    renderTodos();
+    saveTodo();
+}
+buttonElement.onclick = addTodo;
+
+function removeTodo(pos) {
+    todos.splice(pos, 1);
+
+    renderTodos();
+    saveTodo()
+}
+
+function saveTodo() {
+    localStorage.setItem('list_todos', JSON.stringify(todos))
+}
